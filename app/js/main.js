2703a2348b735a1ab71e837bda27c3bba9a4d964
abina nepal <nepal.abina@gmail.com>
2014-02-12 18:30:26 -0500

    added home button and playlist button js

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,13 @@
+$('#playlist-button').on('click', function() {
+	$('#playlist').toggleClass('hidden')
+	$('#home-page').toggleClass('hidden')
 
+})
+$('.home-button').on('click', function() {
+	$('#playlist').toggleClass('hidden')
+	$('#home-page').toggleClass('hidden')
+
+})
+$('#menu-button').on('click', function() {
+	$('#menu').toggleClass('hidden')
+})
