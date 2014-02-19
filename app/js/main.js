803ba45fa94e94ceaaa16f2a94868b0fbce77c69
abina nepal <nepal.abina@gmail.com>
2014-02-19 14:55:00 -0500

    color, home button

--- app/js/main.js
+++ app/js/main.js
@@ -3,11 +3,16 @@ $('#playlist-button').on('click', function() {
 	$('#home-page').toggleClass('hidden')
 
 })
-$('.home-button').on('click', function() {
+$('.back-button').on('click', function() {
 	$('#playlist').toggleClass('hidden')
 	$('#home-page').toggleClass('hidden')
 
 })
 $('#menu-button').on('click', function() {
 	$('#menu').toggleClass('hidden')
+})
+
+$('.home-button').on('click', function() {
+	$('#home-page').toggleClass
+
 })
