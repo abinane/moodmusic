c8f9ed84b0aa308d862ddd4f76d1ee77b7011671
abina nepal <nepal.abina@gmail.com>
2014-02-26 17:45:43 -0500

    first slider is working

--- app/js/main.js
+++ app/js/main.js
@@ -20,5 +20,41 @@ $('#menu-button').on('click', toggleMenu)
 
 $('.home-button').on('click', goHome)
 
+$('#mood-slider').sly({
+	horizontal: 1,
+	itemNav: 'forceCentered',
+	scrollBy: 1,
+})
 
+
+
+// // Trying out sly.js
+// $('#mood-slider').sly({
+// 	horizontal: 1,
+// 	itemNav: 'basic',
+// 	smart: 1,
+// 	activateOn: 'click',
+// 	mouseDragging: 1,
+// 	touchDragging: 1,
+// 	releaseSwing: 1,
+// 	startAt: 3,
+// 	// scrollBar: $wrap.find('.scrollbar'),
+// 	scrollBy: 1,
+// 	// pagesBar: $wrap.find('.pages'),
+// 	activatePageOn: 'click',
+// 	speed: 300,
+// 	elasticBounds: 1,
+// 	easing: 'easeOutExpo',
+// 	dragHandle: 1,
+// 	dynamicHandle: 1,
+// 	clickBar: 1,
+
+// 	// Buttons
+// 	// forward: $wrap.find('.forward'),
+// 	// backward: $wrap.find('.backward'),
+// 	// prev: $wrap.find('.prev'),
+// 	// next: $wrap.find('.next'),
+// 	// prevPage: $wrap.find('.prevPage'),
+// 	// nextPage: $wrap.find('.nextPage')
+// })
 
