f363a47830a83ca35fee2a31b81b91087f3dc936
abina nepal <nepal.abina@gmail.com>
2014-03-20 15:36:25 -0400

    loaded tags from spreadsheet/globally
    relates to #4

--- app/js/main.js
+++ app/js/main.js
@@ -1,49 +1,4 @@
-var moodTags = [
-	'appreciative',
-	'break up',
-	'chilling',
-	'clubbing',
-	'dance',
-	'doubtful',
-	'down south',
-	'dreamy',
-	'driving',
-	'empowered',
-	'excited',
-	'gansta',
-	'getting ready',
-	'hanging out',
-	'happy',
-	'in love',
-	'independent',
-	'insipred',
-	'invincible',
-	'lonely',
-	'lost in jamaica',
-	'love',
-	'melancholy',
-	'missing someone',
-	'motivated',
-	'optimistic',
-	'powerful',
-	'racy',
-	'rastaman',
-	'regretful',
-	'road trip',
-	'romantic',
-	'sad',
-	'successful',
-	'summer',
-	'sweet',
-	'thug',
-	'wishful'
-];
-
-$('.slider-container').each(function( i, el ) {
-	var $el = $( el );
-	$el.find('ul')
-		.html('<li>'+moodTags.join('</li> <li>')+'</li>');
-}) 
+var moodTags = []; 
 
 function switchScreens() {
 	$('#home-page').toggleClass('hidden')
@@ -66,6 +21,22 @@ $('.back-button, #playlist-button')
 $('#menu-button').on('click', toggleMenu);
 
 $('.home-button').on('click', goHome);
+
+var splitTags = function(moodObject) {
+	var moods 
+	if (moodObject !== null) {
+		moods = moodObject.split (',')
+	}
+	else {
+		moods = ['untagged']
+	}
+
+	return moods
+}
+
+var formatTag = function(moodTag) {
+	return moodTag.trim().toLowerCase()
+} 
 
 var sliderOptions = {
 	horizontal: 1,
@@ -91,8 +62,6 @@ var sliderElementTwo = $('.slider-container').last()
 var sliderOne = new Sly( sliderElementOne, sliderOptions, callbackMap );
 var sliderTwo = new Sly( sliderElementTwo, sliderOptions, callbackMap );
 
-sliderOne.init()
-sliderTwo.init()
 
 function play() {
 	var moodOne = moodTags[sliderOne.rel.activeItem];
@@ -105,48 +74,38 @@ function play() {
 $('#play-button').on('click', play)
 
 
-// var ds = new Miso.Dataset({
-//   key : "0Auivztrde1nqdGY2SGU3NGNRbTRLYXk5cG0yTEg0SGc",
-//   worksheet : "1",
-//   importer: Miso.Dataset.Importers.GoogleSpreadsheet,
-//   parser : Miso.Dataset.Parsers.GoogleSpreadsheet
-// });
-// ds.fetch({
-//   success : function() {
-//     console.log(ds.columnNames())
-//   },
-//   error : function() {
-//     // your error callback here!
-//   }
-// });
 
-// // Trying out sly.js
-// $('#mood-slider').sly({
-// 	horizontal: 1,
-// 	itemNav: 'basic',
-// 	smart: 1,
-// 	activateOn: 'click',
-// 	mouseDragging: 1,
-// 	touchDragging: 1,
-// 	releaseSwing: 1,
-// 	startAt: 3,
-// 	// scrollBar: $wrap.find('.scrollbar'),
-// 	scrollBy: 1,
-// 	// pagesBar: $wrap.find('.pages'),
-// 	activatePageOn: 'click',
-// 	speed: 300,
-// 	elasticBounds: 1,
-// 	easing: 'easeOutExpo',
-// 	dragHandle: 1,
-// 	dynamicHandle: 1,
-// 	clickBar: 1,
+var songData = new Miso.Dataset({
+  key : "0Auivztrde1nqdGY2SGU3NGNRbTRLYXk5cG0yTEg0SGc",
+  worksheet : "1",
+  importer : Miso.Dataset.Importers.GoogleSpreadsheet,
+  parser : Miso.Dataset.Parsers.GoogleSpreadsheet
+});
+songData.fetch({
+  success : function() {
+   	moodTags = _.chain( songData.column('Mood Tag').data)
+		.map (splitTags)
+		.flatten()
+		.map (formatTag)
+		.unique ()
+		.value();
+	$('.slider-container').each(function( i, el ) {
+		var $el = $( el );
+		$el.find('ul')
+			.html('<li>'+moodTags.join('</li> <li>')+'</li>');
+	})
+	sliderOne.init()
+	sliderTwo.init()
 
-// 	// Buttons
-// 	// forward: $wrap.find('.forward'),
-// 	// backward: $wrap.find('.backward'),
-// 	// prev: $wrap.find('.prev'),
-// 	// next: $wrap.find('.next'),
-// 	// prevPage: $wrap.find('.prevPage'),
-// 	// nextPage: $wrap.find('.nextPage')
-// })
+	// initialize!
+  },
+  error : function() {
+    // your error callback here!
+  }
+});
+
+
+
+
+
 
